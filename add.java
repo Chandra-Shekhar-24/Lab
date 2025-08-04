7f1f55a0e9c4f1eab1d73eadbe156adb6af47583
Chandra-Shekhar-24 <chandrashekharbansal.2006@gmail.com>
2025-08-04 12:38:05 +0530

    Updated Git.java

--- add.java
+++ add.java
@@ -2,7 +2,8 @@ public class add{
     public static void main(String[] args) {
         int a = 5;
         int b = 10;
-        int c = a+b;
-        System.out.println(c);
+        int c = 15;
+        int d = a+b+c;
+        System.out.println(d);
     }
 }
